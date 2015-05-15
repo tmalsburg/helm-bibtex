888ff2ee33992d86148638809f0d7bae9f18a89c
Titus von der Malsburg <malsburg@posteo.de>
2015-05-15 14:33:56 -0700

    Synchronized features and news with README.

--- helm-bibtex.el
+++ helm-bibtex.el
@@ -28,35 +28,20 @@
 ;; - 05/14/2015: Added support for multiple PDF directories.
 ;; - 02/23/2015: Added a workaround for a bug in Emacs 24.3.1.  If you
 ;;   didn't see any publications, this should fix it.
-;; - 01/11/2015:
-;;   - Added resolution of cross-references.  Missing fields are
-;;     imported from the cross-referenced entry (useful for conference
-;;     abstracts).
-;;   - Editors are shown when there are no authors (useful for
-;;     conference proceedings).
-;;   - Fields that should be ignored when exporting BibTeX can be
-;;     configured via `helm-bibtex-no-export-fields' (useful to keep
-;;     private notes private).
-;; - 01/29/2015: Better formatting of plain text references
-;;   following the APA style guide.
-;; - 01/16/2015: The parsed bibliography is now cached which can
-;;   dramatically improve responsiveness with larger bibliographies.
+;; See NEWS.org for old news.
 ;;
 ;; Key features:
 ;; - Quick access to your bibliography from within Emacs
+;; - Tightly integrated workflows
 ;; - Provides instant search results as you type
+;; - Powerful search expressions
+;; - Open the PDFs, URLs, or DOIs associated with an entry
+;; - Insert LaTeX cite commands, Ebib links, or Pandoc citations,
+;;   BibTeX entries, or plain text references at point, attach PDFs to
+;;   emails
+;; - Attach notes to publications
 ;; - Quick access to online bibliographic databases such as Pubmed,
 ;;   arXiv, Google Scholar, Library of Congress, etc.
-;; - Support for multiple BibTeX files
-;; - Open the PDF associated with an entry
-;; - Open the URL or DOI of an entry in the browser
-;; - Insert LaTeX cite command, ebib link, or pandoc citation
-;;   depending on document type
-;; - Insert BibTeX entry or plain text reference at point (useful when
-;;   sharing BibTeX with colleagues via email)
-;; - Attach PDF of entry to an email.
-;; - Add notes to an entry
-;; - Edit selected entry
 ;;
 ;; See the github page for details:
 ;;
