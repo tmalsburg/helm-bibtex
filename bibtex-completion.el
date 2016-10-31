46285116549fec9933fd035067773f5084937526
Titus von der Malsburg <malsburg@posteo.de>
2016-10-31 10:58:03 +0100

    Amend: Also added a save-restriction.

--- bibtex-completion.el
+++ bibtex-completion.el
@@ -530,9 +530,10 @@ find a PDF file."
                             (f-file? bibtex-completion-notes-path)
                             (with-current-buffer (find-file-noselect bibtex-completion-notes-path)
                               (save-excursion
-                                (widen)
-                                (goto-char (point-min))
-                                (re-search-forward (format bibtex-completion-notes-key-pattern entry-key) nil t)))))
+                                (save-restriction
+                                  (widen)
+                                  (goto-char (point-min))
+                                  (re-search-forward (format bibtex-completion-notes-key-pattern entry-key) nil t))))))
                       (cons (cons "=has-note=" bibtex-completion-notes-symbol) entry)
                     entry))
            ; Remove unwanted fields:
