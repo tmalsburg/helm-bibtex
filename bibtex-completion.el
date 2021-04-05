6ed10b99b2a68d2b48fd911551dcee34530e2d6e
Bruce D'Arcus <bdarcus@gmail.com>
2021-04-05 17:34:19 -0400

    bump version to 1.1.0

--- bibtex-completion.el
+++ bibtex-completion.el
@@ -4,7 +4,7 @@
 ;;         Justin Burkett <justin@burkett.cc>
 ;; Maintainer: Titus von der Malsburg <malsburg@posteo.de>
 ;; URL: https://github.com/tmalsburg/helm-bibtex
-;; Version: 1.0.0
+;; Version: 1.1.0
 ;; Package-Requires: ((parsebib "1.0") (s "1.9.0") (dash "2.6.0") (f "0.16.2") (cl-lib "0.5") (biblio "0.2") (emacs "26.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
